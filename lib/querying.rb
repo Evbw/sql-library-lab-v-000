973216518a277a0ecb04535ee5aed0ed8d9779c7
Everett <everett.bowline@gmail.com>
2018-05-30 02:31:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT name FROM series JOIN books ON books.series_id = series.id JOIN character_books ON books.id =  characters.books_id GROUP BY species ORDER BY count(species) DESC LIMIT 1"
+  "SELECT name FROM series JOIN books ON books.series_id = series.id JOIN character_books ON character_books.id =  character_books_id GROUP BY species ORDER BY count(species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
