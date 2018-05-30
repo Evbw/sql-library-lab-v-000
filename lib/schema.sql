79aa1e0afe8ec0c87cf103305e284ef8a72d9ae1
Everett <everett.bowline@gmail.com>
2018-05-30 01:23:43 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id TEXT, sub_genre_id INTEGER);
 
-CREATE TABLE sub-genre (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE sub_genre (id INTEGER PRIMARY KEY, name TEXT);
 
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 
